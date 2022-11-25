2c07d473d83d992f2f21359186584b028a5420d2
SamienShaheed <92803328+SamienShaheed@users.noreply.github.com>
2022-11-25 18:18:17 +0800

    Create Main Menu + Connect Menu to game
    Connect the Start Game button in Main Menu to the game. Organize and comment out unused code.

--- SoftwareMaintenance/main/java/com/example/demo/Controller.java
+++ SoftwareMaintenance/main/java/com/example/demo/Controller.java
@@ -1,5 +1,14 @@
 package com.example.demo;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
+public class Controller extends Main {
+    @FXML
+    public void handlestartbtn(ActionEvent event) {
+        Stage currentStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        startGame(currentStage);
+    }
 }
--- SoftwareMaintenance/main/java/com/example/demo/Main.java
+++ SoftwareMaintenance/main/java/com/example/demo/Main.java
@@ -1,32 +1,32 @@
 package com.example.demo;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
+//import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.ButtonType;
-import javafx.scene.input.MouseEvent;
+//import javafx.scene.control.*;
+//import javafx.scene.control.ButtonType;
+//import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Control;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
+//import javafx.scene.control.Button;
 import javafx.scene.shape.Rectangle;
 
 import java.io.IOException;
-import java.util.Optional;
+//import java.util.Optional;
 import java.util.Scanner;
 
 public class Main extends Application {
     static final int WIDTH = 600;
     static final int HEIGHT = 600;
-    private Group gameRoot = new Group();
-    private Scene gameScene = new Scene(gameRoot, WIDTH, HEIGHT, Color.rgb(255, 255, 255));
     private static Scanner input = new Scanner(System.in);
-    private AnchorPane mainMenu;
-    private Stage primaryStage;
+    private Group gameRoot = new Group();
+    private Scene gameScene;
+    public Stage primaryStage;
 
     public void setGameScene(Scene gameScene) {
         this.gameScene = gameScene;
@@ -48,26 +48,26 @@ public class Main extends Application {
     private void showMainMenu() throws IOException {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(Main.class.getResource("MainMenu.fxml"));
-        mainMenu = loader.load();
+        AnchorPane mainMenu = loader.load();
         Scene menu = new Scene(mainMenu);
         primaryStage.setScene(menu);
         primaryStage.show();
     }
 
-    private void startGame() throws Exception{
+    // Method to start game
+    public void startGame(Stage primaryStage) {
 
         Group menuRoot = new Group();
-        Scene menuScene = new Scene(menuRoot, WIDTH, HEIGHT);
+        //Scene menuScene = new Scene(menuRoot, WIDTH, HEIGHT);
         Group accountRoot = new Group();
-        Scene accountScene = new Scene(accountRoot, WIDTH, HEIGHT, Color.rgb(150, 20, 100, 0.2));
-        Group getAccountRoot = new Group();
-        Scene getAccountScene = new Scene(getAccountRoot, WIDTH, HEIGHT, Color.rgb(200, 20, 100, 0.2));
+        //Scene accountScene = new Scene(accountRoot, WIDTH, HEIGHT, Color.rgb(150, 20, 100, 0.2));
+        //Group getAccountRoot = new Group();
+        //Scene getAccountScene = new Scene(getAccountRoot, WIDTH, HEIGHT, Color.rgb(200, 20, 100, 0.2));
         Group endgameRoot = new Group();
         Scene endGameScene = new Scene(endgameRoot, WIDTH, HEIGHT, Color.rgb(250, 20, 100, 0.2));
-        Group rankRoot = new Group();
-        Scene rankScene = new Scene(rankRoot, WIDTH, HEIGHT, Color.rgb(250, 50, 120, 0.3));
-        BackgroundFill background_fill = new BackgroundFill(Color.rgb(120, 100, 100), CornerRadii.EMPTY, Insets.EMPTY);
-        Background background = new Background(background_fill);
+        //Group rankRoot = new Group();
+        //Scene rankScene = new Scene(rankRoot, WIDTH, HEIGHT, Color.rgb(250, 50, 120, 0.3));
+        //BackgroundFill background_fill = new BackgroundFill(Color.rgb(120, 100, 100), CornerRadii.EMPTY, Insets.EMPTY);
 
 
         Rectangle backgroundOfMenu = new Rectangle(240, 120, Color.rgb(120, 120, 120, 0.2));
