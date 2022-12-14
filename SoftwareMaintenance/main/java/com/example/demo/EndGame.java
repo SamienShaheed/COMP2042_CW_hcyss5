a5b4c85c117273a1e0e25debe82f2ce230d8cf19
SamienShaheed <92803328+SamienShaheed@users.noreply.github.com>
2022-12-14 19:04:06 +0800

    Progress on Leaderboard

--- SoftwareMaintenance/main/java/com/example/demo/EndGame.java
+++ SoftwareMaintenance/main/java/com/example/demo/EndGame.java
@@ -1,29 +1,25 @@
 package com.example.demo;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.ListView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.Optional;
+import java.util.Scanner;
+
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class EndGame extends Main{
     private static EndGame singleInstance = null;
+    private int [] topScores = new int[10];
 
     public static EndGame getInstance(){
         if(singleInstance == null)
@@ -31,6 +27,7 @@ public class EndGame extends Main{
         return singleInstance;
     }
 
+    // Method to save score to external file
     private void saveScore(long score) {
         // Try Catch Block to store scores in a Txt File
         try {
@@ -43,10 +40,62 @@ public class EndGame extends Main{
         }
     }
 
-    public void endGameShow(Group root, Stage primaryStage, long score) {
-        
-        saveScore(score);
+    // Method to update External Text File 
+    private void updateExternalFile(int scores[]) {
+        try {
+            FileWriter leaderBoardUpdate = new FileWriter("Leaderboard.txt");
+            for(int i = 0; i < 5; i++) { // Store array values into file
+                leaderBoardUpdate.append(Integer.toString(scores[i])).append("\n"); 
+            }
+            leaderBoardUpdate.close();
+        } catch (IOException fileErr) {
+            System.out.println("Could not update LeaderBoard");
+            fileErr.printStackTrace();
+        }
+    }
 
+    private void getTopScores() {
+        // Try Catch Block to retrieve scores from external text file
+        try {
+            File leaderBoard = new File("Leaderboard.txt");
+            Scanner fileReader = new Scanner(leaderBoard);
+            int i = 0;
+            while(fileReader.hasNextInt() && i < 10) {
+                topScores[i++] = fileReader.nextInt(); // Store values into array
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Insertion sort to get top scores
+        // Reference: https://www.geeksforgeeks.org/insertion-sort/
+        for(int i=0; i < 10; i++) {
+            int key = topScores[i];
+            int j = i - 1;
+            while(j >= 0 && topScores[j] < key) {
+                topScores[j+1] = topScores[j];
+                j--;
+            }
+            topScores[j+1] = key;
+        }
+
+        // Update the external file after sort
+        updateExternalFile(topScores);
+
+        // Debugging purposes
+        for(int i = 0; i < 10 ; i++) {
+            System.out.print(topScores[i] + " ");
+        }
+    }
+
+    public void endGameShow(Group root, Stage primaryStage, long score) {
+         
+        // Call function to save score to external file
+        saveScore(score);
+        getTopScores();
+        // --------------------------------------Text------------------------------------
+        // Generate Text to Display on screen
         Text text = new Text("Game Over");
         text.setFill(Color.WHITE);
         text.relocate(236, 65);
@@ -64,8 +113,10 @@ public class EndGame extends Main{
         currentScore.relocate(200, 302);
         currentScore.setFont(Font.font("Rockwell", 50));
         root.getChildren().add(currentScore);
-
-        // Button to quit game
+        // ------------------------------------------------------------------------------ 
+        
+        // --------------------------------------Buttons---------------------------------
+        // Generate Buttons to Display on Screen
         Button quitButton = new Button("Quit");
         quitButton.setPrefSize(100,30);
         quitButton.setTextFill(Color.BLACK);
@@ -77,7 +128,7 @@ public class EndGame extends Main{
             alert.setTitle("Quit Dialog");
             alert.setHeaderText("Quit from this page");
             alert.setContentText("Are you sure?");
-
+            
             Optional<ButtonType> result = alert.showAndWait();
             if (result.get() == ButtonType.OK){
                 primaryStage.close(); // Fixed bug where quit button should close window
@@ -89,7 +140,7 @@ public class EndGame extends Main{
         playAgain.setTextFill(Color.BLACK);
         root.getChildren().add(playAgain);
         playAgain.relocate(326, 432);
-
+        
         playAgain.setOnMouseClicked(event -> {
             try {
                 start(primaryStage);
@@ -97,5 +148,6 @@ public class EndGame extends Main{
                 throw new RuntimeException(e);
             }
         });
+        // ------------------------------------------------------------------------------
     }
 }
